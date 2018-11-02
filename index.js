33d5d306e5067de7c028bd0e437db5a06d21b599
Jesse Evans <jesseelizabeth1@gmail.com>
2018-11-02 00:17:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  // 1. figure out how long our password should be 
+  
+  // 2. make a password from the ground up 
+  
+  // 3. keep track of requirements 
 });
