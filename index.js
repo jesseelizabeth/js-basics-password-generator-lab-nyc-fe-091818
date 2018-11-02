eb158a6223f0c6280be4a0a790f178f0601eb3fa
Jesse Evans <jesseelizabeth1@gmail.com>
2018-11-02 00:29:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ window.addEventListener("load", () => {
   //start your code here
   
   // 1. figure out how long our password should be 
+  document.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const input = document.querySelector('input')
+    const passwordLength = parseInt(input.value, 10)
+    let password = "" // start with empty password
+    
+    for (let i = 0; i < passwordLength; i++) {
+      
+    }
+  })
   
   // 2. make a password from the ground up 
   
